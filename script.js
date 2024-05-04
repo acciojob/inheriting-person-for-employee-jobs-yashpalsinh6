92a9fd6206f50a9d8afa38b154ebd1707c0eee1a
yashpalsinh6 <75825134+yashpalsinh6@users.noreply.github.com>
2024-05-04 17:01:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function Person(name, age) {
 	this.age = age;
 }
 Person.prototype.greet = function() {
-	console.log("Hello, my name is " + this.name + ", I am " + this.age + " years old.");
+	console.log("Hello, my name is " + this.name + " and I am " + this.age + " years old.");
 }
 function Employee(name, age, jobTitle) {
 	Person.call(this, name, age);
