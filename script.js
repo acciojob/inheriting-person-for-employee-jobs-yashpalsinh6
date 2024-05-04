742c820217be6cdadf34244c9bc6f583144cc3eb
yashpalsinh6 <75825134+yashpalsinh6@users.noreply.github.com>
2024-05-04 16:57:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // complete this js code
 function Person(name, age) {
-	this._name = name;
-	this._age = age;
+	this.name = name;
+	this.age = age;
 }
 Person.prototype.greet = function() {
 	console.log("Hello, my name is " + this.name + ", I am " + this.age + " years old.");
@@ -16,7 +16,11 @@ Employee.prototype.jobGreet = function() {
 	console.log("Hello, my name is " + this.name + ", I am " + this.age + " years old, and my job title is " + this.jobTitle + ".");
 }
 
+let person1 = new Person("Alice", 25);
+person1.greet(); // This should print: "Hello, my name is Alice, I am 25 years old."
 
+let employee1 = new Employee("Bob", 30, "Manager");
+employee1.jobGreet(); // This should print: "Hello, my name is Bob, I am 30 years old, and my job title is Manager."
 // Do not change code below this line
 window.Person = Person;
 window.Employee = Employee;
